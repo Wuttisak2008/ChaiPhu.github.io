1360058107887fb26ef9d61fafaf77ad703bef70
Wuttisak2008 <164011@wsk.ac.th>
2026-07-11 20:32:52 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
-document.querySelectorAll(".card").forEach(card=>{
-    card.onclick=()=>{
-        alert("เปิดดูผลงาน");
-    }
-});
+function showProject(text){
+    document.getElementById("detail").innerHTML = text;
+    document.getElementById("popup").style.display = "block";
+}
+
+function closePopup(){
+    document.getElementById("popup").style.display = "none";
+}
