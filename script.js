f09cac1a52758d475316608c15a6e002a94ef399
Wuttisak2008 <164011@wsk.ac.th>
2026-07-11 20:17:46 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
+// เมื่อหน้าเว็บโหลดเสร็จ
+document.addEventListener("DOMContentLoaded", function () {
 
+    // เลือกการ์ดผลงานทั้งหมด
+    const cards = document.querySelectorAll(".card");
+
+    // เมื่อคลิกที่การ์ด
+    cards.forEach((card, index) => {
+        card.addEventListener("click", function () {
+            alert("คุณเลือกดูผลงานที่ " + (index + 1));
+        });
+    });
+
+});
